b6f51d4cf039a8637088316fc6bc4effd0ff6695
patzgn <103074854+patzgn@users.noreply.github.com>
2024-05-15 18:50:32 +0200

    Create TrailDto.cs

---
+++ BlazingTrails.Shared/Features/ManageTrails/TrailDto.cs
@@ -0,0 +1,19 @@
+﻿namespace BlazingTrails.Shared.Features.ManageTrails;
+
+public class TrailDto
+{
+	public int Id { get; set; }
+	public string Name { get; set; } = string.Empty;
+	public string Description { get; set; } = string.Empty;
+	public string Location { get; set; } = string.Empty;
+	public int TimeInMinutes { get; set; }
+	public int Length { get; set; }
+
+	public List<RouteInstruction> Route { get; set; } = [];
+
+	public class RouteInstruction
+	{
+		public int Stage { get; set; }
+		public string Description { get; set; } = string.Empty;
+	}
+}
