71283b038a4885d9baad90af3243f2b5b2bb92ca
patzgn <103074854+patzgn@users.noreply.github.com>
2024-06-01 15:55:58 +0200

    Building custom input components with InputBase

--- BlazingTrails.Shared/Features/ManageTrails/TrailDto.cs
+++ BlazingTrails.Shared/Features/ManageTrails/TrailDto.cs
@@ -36,6 +36,10 @@ public class TrailValidator : AbstractValidator<TrailDto>
 			.NotEmpty()
 			.WithMessage("Please enter a location");
 
+		RuleFor(x => x.TimeInMinutes)
+			.GreaterThan(0)
+			.WithMessage("Please enter a time");
+
 		RuleFor(x => x.Length)
 			.GreaterThan(0)
 			.WithMessage("Please enter a length");
